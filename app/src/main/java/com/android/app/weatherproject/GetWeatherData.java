8261cb768cedfbc77d2a6b0904ee0e1286ccd4cd
Dimitris Vranas <dimivranas@gmail.com>
2017-01-18 02:09:35 +0200

    Added AsyncTask class for fetching the data based on current user location

--- app/src/main/java/com/android/app/weatherproject/GetWeatherData.java
+++ app/src/main/java/com/android/app/weatherproject/GetWeatherData.java
@@ -1,5 +1,6 @@
 package com.android.app.weatherproject;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -10,13 +11,18 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class GetWeatherData extends AsyncTask {
+public class GetWeatherData extends AsyncTask<String, Void, Void> {
 
     // Tag for logging reasons
     private static final String LOG_TAG = GetWeatherData.class.getSimpleName();
 
     @Override
-    protected Object doInBackground(Object[] objects) {
+    protected Void doInBackground(String... objects) {
+
+        // If there's no coordinates, there's nothing to do
+        if (objects.length == 0) {
+            return null;
+        }
 
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
@@ -24,10 +30,34 @@ public class GetWeatherData extends AsyncTask {
         // Will contain the response from the server
         String weatherJsonString = null;
 
+        String language = "el";
+        String units = "si";
+        String exclude = "flags,hourly";
+
         try {
 
             // Set up the url
-            URL url = new URL("https://api.forecast.io/forecast/61ad0962a6f490073559a7b83f40ed2c/39.6383,22.4159?lang=el&units=si&exclude=flags,hourly");
+            // URL url = new URL("https://api.forecast.io/forecast/61ad0962a6f490073559a7b83f40ed2c/39.6383,22.4159?lang=el&units=si&exclude=flags,hourly");
+
+            // Construct the URL for the query with specified users location
+            final String BASE_WEATHER_URL = "https://api.forecast.io/forecast/";
+            final String LANGUAGE = "lang";
+            final String LATLON = objects[0] + "," + objects[1];
+            final String UNITS = "units";
+            final String EXCLUDE = "exclude";
+
+            Uri builtUri = Uri.parse(BASE_WEATHER_URL).buildUpon()
+                    .appendPath(BuildConfig.DARK_SKY_API_KEY)
+                    .appendPath(LATLON)
+                    .appendQueryParameter(LANGUAGE, language)
+                    .appendQueryParameter(UNITS, units)
+                    .appendQueryParameter(EXCLUDE, exclude)
+                    .build();
+
+            URL url = new URL(builtUri.toString());
+            Log.v(LOG_TAG, "The constructed URL is " + url);
+
+
 
             // Create the request and open the connection
             urlConnection = (HttpURLConnection) url.openConnection();
--- app/src/main/java/com/android/app/weatherproject/WeatherFragment.java
+++ app/src/main/java/com/android/app/weatherproject/WeatherFragment.java
@@ -1,8 +1,19 @@
 package com.android.app.weatherproject;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +24,14 @@ import android.view.ViewGroup;
  */
 public class WeatherFragment extends Fragment {
 
+    // Root of the layout of fragment
+    private View mLayout;
+
+    // Tag for logging reasons
+    private static final String LOG_TAG = WeatherFragment.class.getSimpleName();
+
+    // An app defined request constant. The callback method gets the result of the request
+    final private int MY_REQUEST_ACCESS_COARSE_LOCATION = 100;
 
     public WeatherFragment() {
         // Required empty public constructor
@@ -25,11 +44,85 @@ public class WeatherFragment extends Fragment {
         // Inflate the layout for this fragment
         View fragmentView = inflater.inflate(R.layout.fragment_weather, container, false);
 
-        GetWeatherData getWeatherData = new GetWeatherData();
-        getWeatherData.execute();
+        LocationManager locationManager = (LocationManager) getActivity()
+                .getSystemService(Context.LOCATION_SERVICE);
 
+        // Check if the permission for location is granted by the user
+        int permissionCheck = ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_REQUEST_ACCESS_COARSE_LOCATION);
+        } else {
+            Log.v(LOG_TAG, "PERMISSION LOCATION IS GRANTED");
+            // Register the listener with the Location Manager to receive location updates
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        }
+
+        Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        String lat = String.valueOf(lastKnownLocation.getLatitude());
+        String lon = String.valueOf(lastKnownLocation.getLongitude());
+        Log.i(LOG_TAG, "The latitute is" + lat);
+        Log.i(LOG_TAG, "The longitute is" + lon);
+
+        GetWeatherData getWeatherData = new GetWeatherData();
+        getWeatherData.execute(lat, lon);
 
         return fragmentView;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_REQUEST_ACCESS_COARSE_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i(LOG_TAG, "LOCATION permission is granted.");
+                } else {
+                    Log.i(LOG_TAG, "LOCATION permission was not granted.");
+                    Snackbar.make(mLayout, R.string.location_permission_not_granted,
+                            Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    // Define a listener that responds to Location updates
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            // Called when new location is found by the network location provider
+            extractCoordinatesFromLocation(location);
+            Log.v(LOG_TAG, "EXTRACT COORDINATES IS CALLED");
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    public String[] extractCoordinatesFromLocation(Location userLocation) {
+
+        String userLat = String.valueOf(userLocation.getLatitude());
+        String userLon = String.valueOf(userLocation.getLongitude());
+
+        String[] userLatLon = new String[] {userLat, userLon};
+
+        return userLatLon;
     }
 
 }
